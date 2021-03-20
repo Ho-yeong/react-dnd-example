08a03ef6e0c8dfabbd471d480c34ea91a2164618
simon <ghdud0503@gmail.com>
2021-03-21 01:09:09 +0900

    first commit

---
+++ client/src/components/Col.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Col = ({ isOver, children }) => {
+  const className = isOver ? 'highlight-region' : '';
+
+  return <div className={`col${className}`}>{children}</div>;
+};
